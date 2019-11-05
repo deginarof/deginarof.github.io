d2d9ce05c5c8ea7be8d427a49d40e60050f2243e
deginarof <deginapupt@hotmail.com>
2019-11-04 23:58:29 -0300

    week6

--- Week 6/windchill.js
+++ Week 6/windchill.js
@@ -10,7 +10,7 @@ let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
 windchill = Math.round(windchill);
 //console.log(windchill);
 if (tempNumber <= 50 && speedNumber > 3) {
-    document.getElementById("chill").textContent = "Wind Chill is" + windchill + "\xB0F";
+    document.getElementById("chill").textContent = "Wind Chill is " + windchill + "\xB0F";
 } else {
     document.getElementById("chill").textContent = "No Wind Chill Today"
 }

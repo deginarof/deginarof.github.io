35d5573b5c0ac567d08428f4028fb909089729b4
deginarof <deginapupt@hotmail.com>
2019-11-04 23:48:19 -0300

    week6

--- Week 6/windchill.js
+++ Week 6/windchill.js
@@ -1,23 +1,16 @@
-/* Input: The user will write the temperature and the wind speed.
-                         * Processing:call the function windChilli to do the calculation and return the result to 
-                         a variable in te first function.
-                         * Output: Show the result to the user.
-                         */
-// Get the values from the user.
-function doImputOutput() {
-    var t = parseFloat(document.getElementById("Temperature").value);
-    var windS = parseFloat(document.getElementById("WindSpeed").value);
-    var processing = windChill(t, windS);
+<!--WindChill Calculation goes here-->
 
-    // Show the results.
-    document.getElementById("output").innerHTML = processing;
-}
-/* where f is the wind chill factor in Fahrenheit, t is the air temperature
- in Fahrenheit, and s is the wind speed in miles per hour at five feet above
-  the ground*/
-function windChill(t, windS) {
-    var Fahrenheit = (35.74 + (0.6215 * t)) -
-        (35.75 * (Math.pow(windS, 0.16))) +
-        (0.4275 * t * (Math.pow(windS, 0.16)));
-    return Fahrenheit.toFixed(2);
+const tempNumber = parseFloat(document.getElementById("temp").textContent);
+//console.log(tempNumber);
+const speedNumber = parseFloat(document.getElementById("speed").textContent);
+//console.log(speedNumber);
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
+    0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+//console.log(windchill);
+windchill = Math.round(windchill);
+//console.log(windchill);
+if (tempNumber <= 50 && speedNumber > 3) {
+    document.getElementById("chill").textContent = "Wind Chill is" + windchill + "\xB0F";
+} else {
+    document.getElementById("chill").textContent = "No Wind Chill Today"
 }

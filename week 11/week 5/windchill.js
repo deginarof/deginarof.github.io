d4be28a20845eee97a3c4c1321c67649f32af3f6
deginarof <deginapupt@hotmail.com>
2019-12-17 23:38:36 -0300

    Week 12

--- week 11/week 5/windchill.js
+++
@@ -1,8 +0,0 @@
-var temp = jsObject.main.temp;
-var windSpeed = jsObject.wind.speed;
-var windChill = 0
-
-if (temp <= 50 && windSpeed >= 3) {
-    windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
-    document.getElementById('chill').textContent = Math.round(windChill);
-}
